1db0764340da5450960738d0eb3964704f295901
Satsuna <jvbade@gmail.com>
2025-08-16 03:35:37 +0800

    new portfolio

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import initScrollReveal from "./scripts/scrollReveal";
+import initTiltEffect from "./scripts/tiltAnimation";
+import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
